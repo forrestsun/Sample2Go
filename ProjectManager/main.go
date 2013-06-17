143682e53a1a520cffe38d11e1cdfdbf771429cf
shenglong.sun <sunsl@live.com>
2013-06-17 11:56:52 +0800

    指针的测试

--- ProjectManager/main.go
+++ ProjectManager/main.go
@@ -14,24 +14,24 @@ type Tomato struct {
 	user_id    int    //用户编号
 }
 
-func AddTomato(list []Tomato, value Tomato) []Tomato {
-	list = append(list, value)
-	return list
+type TasksManager struct {
+	tms []Tomato
+}
+
+func (t *TasksManager) Add(tm *Tomato) {
+	t.tms = append(t.tms, *tm)
 }
 
 func main() {
-	todo_list := []Tomato{}
+	ts := &TasksManager{make([]Tomato, 0)}
+	t0 := &Tomato{"2010-6-6", "9:30", "10:00", "一个测试用的番茄", 1, 1}
+	ts.Add(t0)
 
-	list_count := 1
-	for list_count < 10 {
-		todo_list = AddTomato(todo_list, Tomato{"2010-6-6", "9:30", "10:00", "一个测试用的番茄", list_count, 1})
-		list_count++
-	}
-
-	for _, v := range todo_list {
+	for _, v := range ts.tms {
 		fmt.Println("=====================================================")
 		fmt.Println("Test tomato date is ", v.date)
-		fmt.Println("Test tomato start time is ", v.start_time)		fmt.Println("Test tomato end time is ", v.end_time)
+		fmt.Println("Test tomato start time is ", v.start_time)
+		fmt.Println("Test tomato end time is ", v.end_time)
 		fmt.Println("Test tomato content is", v.content)
 		fmt.Println("Test tomato project id is ", v.project_id)
 	}
